17cc3bec3b4dc7c4c4bbf0fb63751052a9d62b92
13-soham <sohammondal821@gmail.com>
2025-12-09 00:32:29 +0530

    Header Commit

--- FoodHub/src/App.jsx
+++ FoodHub/src/App.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
+import Header from './Components/Header';
 
 
 
 /*
 Planning → 
 
+1) Header
+  → Logo
+  → nav items
+2) Body
+  → search
+  → resturantContent
+  → resturentCard
+3) Footer
+  → Copyright
+  → Links
+  → Address
+  → Contact
+
 */
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Header/>
+    </div>
   )
 }
 
---
+++ FoodHub/src/Components/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import 'remixicon/fonts/remixicon.css'
+import logo from '../Images/foodhub-high-resolution-logo-transparent.png'
+
+const Header = () => {
+  return (
+    <div className='w-screen px-7 py-3'>
+      <div className='border-2 border-black px-5 py-2 flex items-center justify-between'>
+        <div className='h-20 w-20 overflow-hidden flex items-center cursor-pointer'>
+          <img src={logo} alt='FoodHub logo' className='h-full w-auto object-contain' />
+        </div>
+        <div className='flex items-center gap-5'>
+          <h2 className='text-m cursor-pointer'>Home</h2>
+          <h2 className='text-m cursor-pointer'>About Us</h2>
+          <h2 className='text-m cursor-pointer'>Contact Us</h2>
+          <i className='ri-shopping-cart-line text-2xl cursor-pointer'></i>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
